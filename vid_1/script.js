d693d15b18be41092113e9001a4105ccc5f5dc2c
Debanjan110d <debanjangamedu@gmail.com>
2025-11-22 22:23:09 +0530

    add initial HTML, CSS, and JavaScript files for GSAP animation tutorial

---
+++ vid_1/script.js
@@ -0,0 +1,24 @@
+gsap.to(
+    "#box1",//Have to mention the same name as we write in css 
+    {
+        x: 1000,//? Move the box 1000px to the right (x axis)
+        duration: 3, //* Move the box in 3 seconds
+        delay: 2, //* Move the box after 2 seconds
+        rotate: 360,//? Rotate the box 360 degrees
+        backgroundColor: "pink",
+        borderRadius: "50%",//? Make the box round
+        scale: 0.5// It will make the box half the size
+
+
+    }
+)
+gsap.from(
+    "#box2",//Have to mention the same name as we write in css 
+    {
+        x: 1000,//? Move the box 1000px to the right (x axis)
+        duration: 3, //* Move the box in 3 seconds
+        delay: 2, //* Move the box after 2 seconds
+
+
+    }
+)

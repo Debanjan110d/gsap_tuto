8369434153cf264bdd3827807a7107f9304bec5d
Debanjan110d <debanjangamedu@gmail.com>
2025-12-01 22:52:36 +0530

    feat: Add GSAP elastic string animation with SVG path manipulation, mouse interaction, and accompanying learning notes.

--- vid_3/script.js
+++ vid_3/script.js
@@ -6,10 +6,38 @@ var finalPath = "M 10 100 Q 500 190 990 100";
 var string = document.querySelector("#string")
 
 
-string.addEventListener("mouseenter", function (dets) {
-    console.log(dets);
+// Add event listener for mouse movement over the string container
+string.addEventListener("mousemove", function (dets) {
+    // Get the bounding rectangle of the container to calculate relative coordinates
+    // This fixes the issue where the string wouldn't follow correctly if the container wasn't at the top-left of the page
+    var rect = string.getBoundingClientRect();
+
+    // Calculate x and y relative to the container
+    var relativeX = dets.clientX - rect.left;
+    var relativeY = dets.clientY - rect.top;
+
+    // Update the path string with the new control point coordinates
+    // M 10 100: Start point
+    // Q ${relativeX} ${relativeY}: Quadratic Bezier curve control point (follows mouse)
+    // 990 100: End point
+    path = `M 10 100 Q ${relativeX} ${relativeY} 990 100`;
+
+    // Animate the path using GSAP
+    gsap.to("svg path", {
+        attr: { d: path }, // Update the 'd' attribute of the path
+        duration: 0.3,     // Short duration for responsive follow
+        ease: "power3.out" // Smooth easing
+    })
 })
 
+// Add event listener for when the mouse leaves the container
 string.addEventListener("mouseleave", function () {
-    console.log("left");
+    // Animate back to the initial straight/curved line
+    gsap.to("svg path", {
+        attr: { d: initialPath },
+        duration: 1.5,             // Longer duration for the wobble effect
+        ease: "elastic.out(1, 0.2)" // Elastic ease for the bounce/wobble
+    })
 })
+
+

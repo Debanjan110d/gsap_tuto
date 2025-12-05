bbcceb204e4c2468e8bfc27f415a9d450ce44976
Debanjan110d <debanjangamedu@gmail.com>
2025-12-05 23:40:02 +0530

    feat: Implement an animated side navigation menu with GSAP.

--- vid_5/script.js
+++ vid_5/script.js
@@ -1,1 +1,49 @@
-// GSAP Code here
+gsap.from("#nav", {
+    y: -100,
+    duration: 1,
+    ease: "power3.out"
+})
+
+//* Query Selectors
+
+const nav = document.querySelector("#nav");
+const side = document.querySelector("#side");
+const menu = document.querySelector(".ri-menu-line");
+const close = document.querySelector(".ri-close-line");
+
+//? Timeline
+
+const tl = gsap.timeline({});
+
+
+tl.to("#side", {
+    right: 0,
+    duration: 1,
+    ease: "power3.out"
+})
+
+tl.from("#side i", {
+    x: -100,
+    duration: 0.6,
+    ease: "power3.out",
+    opacity: 0
+})
+
+tl.from("#side li", {
+    x: 100,
+    duration: 0.6,
+    ease: "power3.out",
+    stagger: 0.3,
+    opacity: 0
+})
+
+tl.pause();
+
+menu.addEventListener("click", () => {
+    tl.play();
+})
+
+close.addEventListener("click", () => {
+    tl.reverse();
+})
+
